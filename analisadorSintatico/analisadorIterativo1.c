98aa101b9e9390a17acfb5ef72b07fcadb483d1d
Brendon <brendonqueiros@hotmail.com>
2022-11-20 01:21:33 -0300

    iniciando estrutura da arvore de análise/ arvore compactada

--- analisadorSintatico/analisadorIterativo1.c
+++ analisadorSintatico/analisadorIterativo1.c
@@ -52,7 +52,7 @@ q1:
     printf("stackTop == %c\n", stack[top]);
 
 // ! p1: S → M
-    if ((token == 'M') && (stack[top] == 'S'))
+    if ((token == 'm') && (stack[top] == 'S'))
     {
         pop();
         push('M');
@@ -99,7 +99,7 @@ q1:
         push(';');
         push(')');
         push('E');
-        push('())');
+        push(')');
         push('r');
         push(';');
         push('C');
@@ -113,41 +113,349 @@ q1:
     else if ((token == 'm') && (stack[top] == 'M'))
     {
         pop();
-        
-        push(')');
-        push('C');
+        push('}');
         push(';');
+        push(')');
         push('E');
-        push('r');
         push('(');
+        push('r');
+        push(';');
+        push('C');
         push('{');
+        push(')');
+        push('(');
         push('m');
         goto q1;
     }
+    //p7 : E → 0 | 1 | x | y | (EXE)
+    else if ((token == '0') && (stack[top] == 'E'))
+    {
+        pop();
+        push('0');
+        goto q1;
+    }
+    else if ((token == '1') && (stack[top] == 'E'))
+    {
+        pop();
+        push('1');
+        goto q1;
+    }
+    else if ((token == 'x') && (stack[top] == 'E'))
+    {
+        pop();
+        push('x');
+        goto q1;
+    }
+    else if ((token == 'y') && (stack[top] == 'E'))
+    {
+        pop();
+        push('y');
+        goto q1;
+    }
+    //p11: E -> (EXE)
+    else if ((token == '(') && (stack[top] == 'E'))
+    {
+        pop();
+        push(')');
+        push('E');
+        push('X');
+         push('E');
+        push('(');
+        goto q1;
+    }
+    //p:12 X → + | - | * | /
+    else if ((token == '+') && (stack[top] == 'X'))
+    {
+        pop();
+        push('+');
+        goto q1;
+    }
+    else if ((token == '-') && (stack[top] == 'X'))
+    {
+        pop();
+        push('-');
+        goto q1;
+    }
+    else if ((token == '*') && (stack[top] == 'X'))
+    {
+        pop();
+        push('*');
+        goto q1;
+    }
+    else if ((token == '/') && (stack[top] == 'X'))
+    {
+        pop();
+        push('/');
+        goto q1;
+    }
 
-    // else if ((token == '(')&&(stack[top]=='S')) { pop(); push(')'); push('S'); push('X');push('S');push('('); goto q1; }
-    // else if ((token == '+')&&(stack[top]=='X')) { pop(); push('+'); goto q1; }
-    // else if ((token == '-')&&(stack[top]=='X')) { pop(); push('-'); goto q1; }
-    // else if ((token == '*')&&(stack[top]=='X')) { pop(); push('*'); goto q1; }
-    // else if ((token == '/')&&(stack[top]=='X')) { pop(); push('/'); goto q1;}
-    // else if ((token == '(')&&(stack[top]=='(')) { pop(); posicao++;goto q1;}
-    // else if ((token == ')')&&(stack[top]==')')) { pop();posicao++;goto q1; }
-    // else if ((token == '+')&&(stack[top]=='+')) { pop();posicao++;goto q1; }
-    // else if ((token == '-')&&(stack[top]=='-')) { pop();posicao++;goto q1;  }
-    // else if ((token == '*')&&(stack[top]=='*')) { pop();posicao++;goto q1; }
-    // else if ((token == '/')&&(stack[top]=='/')) { pop();posicao++;goto q1; }
-    // else if ((token == '0')&&(stack[top]=='0')) { pop();posicao++;goto q1; }
-    // else if ((token == '1')&&(stack[top]=='1')) { pop();posicao++;goto q1;  }
+    //p16: C -> h=E | i=E | j=E | k=E | z=E
+    else if ((token == 'h') && (stack[top] == 'C'))
+    {
+        pop();
+        push('E');
+        push('=');
+        push('h');
+        goto q1;
+    }
+    else if ((token == 'i') && (stack[top] == 'C'))
+    {
+        pop();
+        push('E');
+        push('=');
+        push('i');
+        goto q1;
+    }
+    else if ((token == 'j') && (stack[top] == 'C'))
+    {
+        pop();
+        push('E');
+        push('=');
+        push('j');
+        goto q1;
+    }
+    else if ((token == 'k') && (stack[top] == 'C'))
+    {
+        pop();
+        push('E');
+        push('=');
+        push('k');
+        goto q1;
+    }
+    else if ((token == 'z') && (stack[top] == 'C'))
+    {
+        pop();
+        push('E');
+        push('=');
+        push('z');
+        goto q1;
+    }
+
+    // p21: C -> (EXE)
+    else if ((token == '(') && (stack[top] == 'C'))
+    {
+        pop();
+        push(')');
+        push('E');
+        push('X');
+        push('E');
+        push('(');
+        goto q1;
+    }
+    //p22: C -> w(E){ C; }
+    else if ((token == 'w') && (stack[top] == 'C'))
+    {
+        pop();
+        push('}');
+        push(';');
+        push('C');
+        push('{');
+        push(')');
+        push('E');
+        push('(');
+        push('w');
+        goto q1;
+    }
+
+    // p23:  C -> f(E){ C; }
+    else if ((token == 'f') && (stack[top] == 'C'))
+    {
+        pop();
+        push('}');
+        push(';');
+        push('C');
+        push('{');
+        push(')');
+        push('E');
+        push('(');
+        push('f');
+        goto q1;
+    }
+
+    // p24: C -> o(E; E; E){ C; }
+    else if ((token == 'o') && (stack[top] == 'C'))
+    {
+        pop();
+        push('}');
+        push(';');
+        push('C');
+        push('{');
+        push(')');
+        push('E');
+        push(';');
+        push('E');
+        push(';');
+        push('E');
+        push('(');
+        push('o');
+        goto q1;
+    }
+
+    //fazer produções que consumam (identifiquem) os terminais m,r,0,1,x,y,(,),+,-,*,/,h,i,j,k,z,w,{,},;,f,=,o
+    else if ((token == 'm') && (stack[top] == 'm'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == 'r') && (stack[top] == 'r'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == '0') && (stack[top] == '0'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == '1') && (stack[top] == '1'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == 'x') && (stack[top] == 'x'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == 'y') && (stack[top] == 'y'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == '(') && (stack[top] == '('))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == ')') && (stack[top] == ')'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == '+') && (stack[top] == '+'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == '-') && (stack[top] == '-'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == '*') && (stack[top] == '*'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == '/') && (stack[top] == '/'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == 'h') && (stack[top] == 'h'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == 'i') && (stack[top] == 'i'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == 'j') && (stack[top] == 'j'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == 'k') && (stack[top] == 'k'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if ((token == 'z') && (stack[top] == 'z'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if((token == 'w') && (stack[top] == 'w'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if((token == '{') && (stack[top] == '{'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if((token == '}') && (stack[top] == '}'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if((token == ';') && (stack[top] == ';'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if((token == 'f') && (stack[top] == 'f'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if((token == '=') && (stack[top] == '='))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    else if((token == 'o') && (stack[top] == 'o'))
+    {
+        pop();
+        posicao++;
+        goto q1;
+    }
+    
+
+
+
     else if (stack[top] == '\0' && token == '\0')
     {
         printf("palavra consumida");
     }
     else
     {
-        printf("erro\n\n");
+        printf("\nerro Sintático\n\n");
         printf("token == %c\n", token);
         printf("stackTop == %c\n", stack[top]);
         printf("stack == %s\n", stack);
+        // print stack reverse
+        printf("stack reverse == ");
+        for (int i = sizeof(stack); i >= 0; i--)
+        {
+            printf("%c", stack[i]);
+        }
         return 1;
     }
 }
